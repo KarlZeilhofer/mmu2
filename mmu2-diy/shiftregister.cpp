19b2013dc46ff74b23411ac4ba80bff7b513d86b
Karl Zeilhofer <zeilhofer@team14.at>
2018-10-20 06:36:45 +0200

    apply artistic style formatter, template from Qt ...
    https://wiki.qt.io/Qt_Coding_Style

--- mmu2-diy/shiftregister.cpp
+++ mmu2-diy/shiftregister.cpp
@@ -1,24 +1,24 @@
 #include "shiftregister.h"
 
 ShiftRegister::ShiftRegister(uint8_t nrOfBits, PinNr data, PinNr clock, PinNr latch)
-	:N(nrOfBits)
+    :N(nrOfBits)
 {
-	if(N > 32){
-		N = 32;
-	}
-	if(N < 8){
-		N = 8;
-	}
+    if(N > 32) {
+        N = 32;
+    }
+    if(N < 8) {
+        N = 8;
+    }
 
-	data = 0; // clear all bits by default
+    data = 0; // clear all bits by default
 }
 
 void ShiftRegister::writeBit(uint8_t bitNr, bool value)
 {
-	if(value){
-		data |= (1<<bitNr);
-	}else{
-		data &= ~(1<<bitNr);
-	}
+    if(value) {
+        data |= (1<<bitNr);
+    } else {
+        data &= ~(1<<bitNr);
+    }
 }
 
--- mmu2-diy/shiftregister.h
+++ mmu2-diy/shiftregister.h
@@ -7,50 +7,50 @@
 class ShiftRegister
 {
 public:
-	ShiftRegister(uint8_t nrOfBits, PinNr data, PinNr clock, PinNr latch);
+    ShiftRegister(uint8_t nrOfBits, PinNr data, PinNr clock, PinNr latch);
 
-	/**
-	 * @brief setBits
-	 * sets all bits in current data to one, where mask has a 1.
-	 * @param mask
-	 */
-	void setBits(uint32_t mask);
+    /**
+     * @brief setBits
+     * sets all bits in current data to one, where mask has a 1.
+     * @param mask
+     */
+    void setBits(uint32_t mask);
 
-	/**
-	 * @brief clearBits
-	 * sets all bits in current data to zero, where mask has a 1.
-	 * @param mask
-	 */
-	void clearBits(uint32_t mask);
+    /**
+     * @brief clearBits
+     * sets all bits in current data to zero, where mask has a 1.
+     * @param mask
+     */
+    void clearBits(uint32_t mask);
 
-	/**
-	 * @brief writeData
-	 * overwrite current data
-	 * @param data
-	 */
-	void writeData(uint32_t data);
+    /**
+     * @brief writeData
+     * overwrite current data
+     * @param data
+     */
+    void writeData(uint32_t data);
 
-	/**
-	 * @brief transferData
-	 * clocks current data out to the shift registers.
-	 * MSB first.
-	 */
-	void transferData();
+    /**
+     * @brief transferData
+     * clocks current data out to the shift registers.
+     * MSB first.
+     */
+    void transferData();
 
-	/**
-	 * @brief writeBit
-	 * Writes a single bit to the data in RAM
-	 * @param bitNr starting from 0 until N-1
-	 * @param value: 0 or 1
-	 */
-	void writeBit(uint8_t bitNr, bool value);
+    /**
+     * @brief writeBit
+     * Writes a single bit to the data in RAM
+     * @param bitNr starting from 0 until N-1
+     * @param value: 0 or 1
+     */
+    void writeBit(uint8_t bitNr, bool value);
 
-	/// Number of bits, usually 8, 16, 24 or 32 (max.)
-	uint8_t N;
-	uint32_t data;
-	PinNr dataPin;
-	PinNr clockPin;
-	PinNr latchPin;
+    /// Number of bits, usually 8, 16, 24 or 32 (max.)
+    uint8_t N;
+    uint32_t data;
+    PinNr dataPin;
+    PinNr clockPin;
+    PinNr latchPin;
 };
 
 #endif // SHIFTREGISTER_H
